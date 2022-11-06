2b69cd181e477e2bb94829e0f87ea7906e3cb2c7
blackcatzjh <751017205@qq.com>
2022-11-06 20:24:54 +0800

    Initial commit 	new file:   .vscode/launch.json 	new file:   app.py 	new file:   appTest.py

---
+++ appTest.py
@@ -0,0 +1,17 @@
+import fedml
+if __name__ == "__main__":
+    # init FedML framework
+    args = fedml.init()
+
+    # init device
+    device = fedml.device.get_device(args)
+
+    # load data
+    dataset, output_dim = load_data(args)
+
+    # load model (the size of MNIST image is 28 x 28)
+    model = LogisticRegression(28 * 28, output_dim)
+
+    # start training
+    simulator = Simulator(args, device, dataset, model)
+    simulator.run()
